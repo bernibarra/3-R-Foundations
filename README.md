5432bf0152f06f5c41e91e781f106f2fbfb5b5c9
mkfreeman <michael.k.freeman@gmail.com>
2018-03-27 13:24:15 -0700

    Add starter files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# a3-starter
-Starter repo for assignment 3
+# a3-using-data
+In this assignment, you'll complete the instructions in the assignment.R file to demonstrate foundational skills of working with R. When you're finished, please make sure to **submit this GitHub URL to Canvas**   on time for full credit.
---
+++ assignment.r
@@ -0,0 +1,169 @@
+# a3-using-data
+
+# Before you get started, set your working directory using the Session menu
+
+###################### DataFrame Manipulation (20 POINTS) ######################
+
+# Create a vector `first_names` with 5 names in it
+
+
+# Create a vector `math_grades` with 5 hypothetical grades (0 - 100)
+# in a math course (that correspond to the 5 names above)
+
+
+# Create a vector `spanish_grades` with 5 hypothetical grades (0 - 100)
+# in a Spanish course (that correspond to the 5 names above
+
+# Create a data.frame variable `students` by combining
+# the vectors `first_names`, `math_grades`, and `spanish_grades`
+
+
+# Create a variable `num_students` that contains the
+# number of rows in your dataframe `students`
+
+
+# Create a variable `num_courses` that contains the number of columns
+# in your dataframe `students` minus one (b/c of their names)
+
+
+# Add a new column `grade_diff` to your dataframe, which is equal to
+# `students$math_grades` minus `students$spanish_grades`
+
+
+# Add another column `better.at.math` as a boolean (TRUE/FALSE) variable that
+# indicates that a student got a better grade in math
+
+# Create a variable `num_better_at_math` that is the number
+# (i.e., one numeric value) of students better at math
+
+
+# Write your `students` dataframe to a new .csv file inside your data/ directory
+# with the filename `grades.csv`. Make sure *not* to write row names.
+
+
+
+########################### Loading R Data (30 points) #########################
+
+# In this section, you'll work with the `Titanic` data set
+# Which is built into the R environment. You should be able to `View()` it
+# Pay *close attention* to what each column means. Use ?Titanic to read more.
+
+
+# This data set actually loads in a format called a *table*
+# See https://cran.r-project.org/web/packages/data.table/data.table.pdf
+# Use the `is.data.frame()` function to test if it is a table.
+
+
+# You should convert the `Titanic` variable into a data frame;
+# you can use the `data.frame()` function or `as.data.frame()`
+# Be sure to **not** treat strings as factors!
+
+
+# Create a variable `children` that are the *only* the rows of the data frame
+# with information about the number children on the Titanic.
+
+
+# Create a variable `num_children` that is the total number of children.
+# Hint: remember the `sum()` function!
+
+
+# Create a variable `most_lost` which has the *row* with the
+# largest absolute number of losses (people who did not survive).
+# Tip: if you want, you can use multiple statements (lines of code)
+# if you find that helpful to create this variable.
+
+
+# Define a function called `survival_rate()` that takes in a ticket class
+# (e.g., "1st", "2nd") as an argument.This function should return the following
+# sentence that compares the *total survival rate* of adult men vs.
+# "women and children" in that ticketing class. It should read (for example):
+# "Of Crew class, 87% of women and children survived and 22% of men survived.".
+# The approach you take to generating the sentence to return is up to you.
+# A good solution will likely utilize filtering to produce the required data.
+
+
+# Create variables `first_survived`, `second_survived`, `third_survived` and
+# `crew_survived` by passing each class to your function above.
+
+
+########################### Reading in Data (40 points)#########################
+# In this section, we'll read in a .csv file with a tabular row/column layout
+# This is like Microsoft Excel or Google Docs, but without the formatting.
+# The .csv file we'll be working with has the life expectancy
+# for each country in 1960 and 2013. We'll ask real-world questions about the
+# data by writing the code that answers our question.
+
+
+# Using the `read.csv` function, read the life_expectancy.csv file into
+# a variable called `life_expectancy`. Makes sure not to read strings as factors
+
+
+# Determine if `life_expectancy` is a data.frame by using
+# the is.data.frame function. You may also want to View() it.
+
+
+# Create a column `life_expectancy$change` that is the change
+# in life expectancy from 1960 to 2013
+
+
+# Create a variable `most_improved` that is the *name* of the country
+# with the largest gain in life expectancy
+
+
+# Create a variable `num_small_gain` that has the *number* of countries
+# whose life expectance has improved fewer than 5 years between 1960 and 2013
+
+
+# Write a function `country_change()` that takes in a country's name
+# as a parameter, and returns it's change in life expectancy from 1960 to 2013
+
+
+# Using your `country_change()` function, create a variable `sweden_change`
+# that is the change in life expectancy from 1960 to 2013 in Sweden
+
+
+# Define a function `lowest_life_exp_in_region()` that takes in a **region**
+# as an argument, and returns the **name of the country**
+# with the lowest life expectancy in 2013 (in that region)
+
+
+# Using the function you just wrote, create a variable `lowest_in_south_asia`
+# that is the country with the lowest life expectancy in 2013 in South Asia
+
+
+# Write a function `bigger_change()` that takes in two country names
+# as parameters, and returns a sentence that describes which country experienced
+# a larger gain in life expectancy (and by how many years).
+# For example, if you passed the values "China", and "Bolivia" to you function,
+# It would return this:
+# "The country with the bigger change in life expectancy was China (gain=31.9),
+#  whose life expectancy grew by 7.4 years more than Bolivia's (gain=24.5)."
+# Make sure to round your numbers to one digit.
+
+
+# Using your `bigger_change()` function, create a variable `usa_or_france`
+# that describes who had a larger gain in life expectancy (the U.S. or France)
+
+
+# Write your `life_expectancy` data.frame to a new .csv file to your
+# data/ directory with the filename `life_expectancy_with_change.csv`.
+# Make sure not to write row names.
+
+
+############################## Challenge (10 points) ###########################
+# Create a variable `highest_avg_change` that has the name of the region with
+# the highest *average change* in life expectancy between the two time points.
+# To do this, you'll need to *compute the average* change across the countries
+# in each region, and then compare the averages across regions.
+# Feel free to use any library of your choice, or base R functions.
+
+
+# Create a *well labeled* plot (readable title, x-axis, y-axis) showing
+# Life expectancy in 1960 v.s. Change in life expectancy
+# Programmatically save (i.e., with code, not using the Export button)
+# your graph as a .png file in your repo
+# Then, in a comment below, *provide an interpretation* of the relationship
+# you observe. Feel free to use any library of your choice, or base R functions.
+
+
+# Put your interpretation here!
